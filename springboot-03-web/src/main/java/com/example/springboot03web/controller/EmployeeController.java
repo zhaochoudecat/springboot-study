c35b5148884dcb1b5312a9ce901dc5f6977c0546
zhaochoudecat <1115182178@qq.com>
2022-03-17 17:35:36 +0800

    员工修改

--- springboot-03-web/src/main/java/com/example/springboot03web/controller/EmployeeController.java
+++ springboot-03-web/src/main/java/com/example/springboot03web/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -40,6 +41,25 @@ public class EmployeeController {
     public String addEmp(Employee employee){
         //操作
         System.out.println("save=>" + employee);
+        employeeDao.addEmployee(employee);
+        return "redirect:/emps";
+    }
+
+    //员工修改页面
+    @GetMapping("/emp/{id}")
+    public String toUpdateEmp(@PathVariable("id")Integer id,Model model){
+        //查出原来的数据
+        Employee employee = employeeDao.getEmployee(id);
+        model.addAttribute("emp",employee);
+        //查出所有部门的信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+
+        return "emp/update";
+    }
+
+    @PostMapping("/updateEmp")
+    public String updateEmp(Employee employee){
         employeeDao.addEmployee(employee);
         return "redirect:/emps";
     }
